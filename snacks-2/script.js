1008d4de374b63ddc222aa3ae041fbe196c0d79b
lucagrandieri <grandieriluca@gmail.com>
2026-02-05 11:26:13 +0100

    aggiunto e formattato html, aggiunto script in js

---
+++ snacks-2/script.js
@@ -0,0 +1,14 @@
+let firstWord = prompt('Prima parola');
+
+let secondWord = prompt('Seconda parola');
+
+if (firstWord > secondWord) {
+    console.log(firstWord + ' ' + secondWord);
+    
+} else if (secondWord > firstWord) {
+    console.log(secondWord + ' ' + firstWord);
+    
+} else {
+    console.log('Le parole hanno la stessa lunghezza');
+    
+}
