f7881c9f2c2ccdf619d0214ed5fb7a34e95a3043
lucagrandieri <grandieriluca@gmail.com>
2026-02-05 10:47:53 +0100

    corretto sintassi

--- snacks-1/script.js
+++ snacks-1/script.js
@@ -5,7 +5,7 @@ let numberTwo = prompt('Secondo numero');
 if (numberOne > numberTwo) {
     console.log('Il numero più grande è' + ' ' + numberOne);
     
-} else if (numberOne = numberTwo) {
+} else if (numberOne == numberTwo) {
     console.log('I numeri sono uguali');
     
 } else {
