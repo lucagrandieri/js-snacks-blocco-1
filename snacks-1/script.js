592a5d3d30493fa9f1333f004f4aee50868127a3
lucagrandieri <grandieriluca@gmail.com>
2026-02-05 10:38:15 +0100

    creato e formattato file html, svolto il primo snack nel file js

---
+++ snacks-1/script.js
@@ -0,0 +1,11 @@
+let numberOne = prompt('Primo numero');
+
+let numberTwo = prompt('Secondo numero');
+
+if (numberOne > numberTwo) {
+    console.log('Il numero più grande è' + ' ' + numberOne);
+    
+} else {
+    console.log('Il numero più grande è' + ' ' + numberTwo);
+    
+}
