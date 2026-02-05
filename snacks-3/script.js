d7385ae3852f71c976a01dd38d008606f06327c0
lucagrandieri <grandieriluca@gmail.com>
2026-02-05 12:41:03 +0100

    .

--- snacks-3/script.js
+++ snacks-3/script.js
@@ -3,7 +3,7 @@ let sum = 0;
 for (let i = 1; i <= 10; i++) {
     let numbers = Number(prompt('Inserisci numero'));
     sum = sum + numbers
- // sum =+ numbers
+ // sum += numbers
 }
 
 console.log(sum);
